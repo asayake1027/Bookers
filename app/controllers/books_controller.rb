226e9f8775362d870a68ff2ec4f4f708c6d16df5
EC2 Default User <ec2-user@ip-172-31-7-44.ec2.internal>
2021-09-26 09:59:31 +0000

    [Add]コントローラの追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'books/top'
+  get 'books/index'
+  get 'books/show'
+  get 'books/create'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
+  get 'homes/top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
